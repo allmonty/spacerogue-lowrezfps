1443cf02dca2b3008a032be493b37760d2ab7699
Allan <allanbrados@gmail.com>
2016-04-18 01:50:43 -0300

    Implements maps and rescue zone mechanics.

---
+++ Assets/Scripts/Player/ObjectiveControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ObjectiveControl : MonoBehaviour {
+
+    public int numberOfMapsInGame = 3;
+    public int numberOfMapsCaught = 0;
+
+    void OnTriggerStay(Collider col)
+    {
+        if(col.gameObject.layer == LayerMask.NameToLayer("Map"))
+        {
+            numberOfMapsCaught += 1;
+            Destroy(col.gameObject);
+        }
+
+        if(col.gameObject.layer == LayerMask.NameToLayer("RescueZone"))
+        {
+            if(numberOfMapsCaught >= numberOfMapsInGame)
+            {
+                SceneManager.LoadScene("EndGame");
+            }
+        }
+    }
+}
