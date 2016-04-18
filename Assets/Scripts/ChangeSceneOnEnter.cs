6b6476eda349089c6421212a806d7fb08e310cea
Allan <allanbrados@gmail.com>
2016-04-18 01:31:36 -0300

    Implements new scenes: Start, Credits, Dialogue 1, Dialogue 2 and Game Over.

---
+++ Assets/Scripts/ChangeSceneOnEnter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneOnEnter : MonoBehaviour {
+
+    public string nextScene = "StartGame";
+
+	void Update () {
+	    if(Input.GetButton("Submit"))
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+	}
+}
