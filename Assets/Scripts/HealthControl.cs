8d20b770aa6f6dbb4e0b5c9e6c471db773d1a882
Allan <allanbrados@gmail.com>
2016-04-18 00:55:07 -0300

    Player and enemies now have health control. Player's death leads to the game over scene.

---
+++ Assets/Scripts/HealthControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthControl : MonoBehaviour {
+    
+    public int lifePoints = 2;
+
+	public void takeDamage(int dmgPoints)
+    {
+        lifePoints -= dmgPoints;
+
+        if(lifePoints <= 0)
+        {
+            die();
+        }
+    }
+
+    virtual protected void die()
+    {
+        Destroy(gameObject);
+    }
+}
